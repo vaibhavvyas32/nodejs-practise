b28c56a4e8a0d114575193b74c443e26935e3a2f
Vaibhav Vyas <vaibhavvyas32@gmail.com>
2022-07-28 08:50:50 +0530

    request & response demo

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.headers);
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First NodeJS Server</title></head>");
+  res.write("<body>NodeJS Server is running.....</body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(3000);
+server.listen(3000, console.log("Server is running at localhost:3000"));
