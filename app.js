ef9ac84eeebb6b02be1e4c0633df83605e796c7b
Vaibhav Vyas <vaibhavvyas32@gmail.com>
2022-07-28 09:09:35 +0530

    routing requests

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-type", "text/html");
   res.write("<html>");
   res.write("<head><title>First NodeJS Server</title></head>");
