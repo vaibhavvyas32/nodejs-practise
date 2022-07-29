71d35453b82f5b66103fd6034ccb639dd170f158
Vaibhav Vyas <vaibhavvyas32@gmail.com>
2022-07-29 17:42:47 +0530

    parsing request bodies

--- app.js
+++ app.js
@@ -15,13 +15,26 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
+    const body = [];
+    req.on("data", (chunk) => {
+      //Used .on(data) to get the request and parse it into the body.
+      console.log(chunk); //Printing those chunks
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      //This will be executed at the end of the request.
+      const parsedBody = Buffer.concat(body).toString(); //As the output will be a text we used Buffer.concat.toString to make it into a readable form.
+      const message = parsedBody.split("=")[1]; //To see the message given by the user. we have split the left side and made message display only the value in key-value pair
+      fs.writeFileSync("message.txt", message); //using fs write file method to write the message inside the message.txt
+      console.log("Message Added Successfully!"); //For Verification nothing important
+    });
+
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
   }
 
-  res.setHeader("Content-type", "text/html");
+  res.setHeader("Content-type", "text/html"); //Defining our own header of text html
   res.write("<html>");
   res.write("<head><title>First NodeJS Server</title></head>");
   res.write("<body>NodeJS Server is running.....</body>");
