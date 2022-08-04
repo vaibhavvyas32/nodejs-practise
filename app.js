de00aa6ec97683be846f8a5f99976f7eedf6c99c
Vaibhav Vyas <vaibhavvyas32@gmail.com>
2022-08-04 22:32:38 +0530

    added expressJs

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require("http");
 
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //In order to go to the next function we have to use next to tell the middleware to go down.
+  //It follows top to bottom approach.
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  // ...
+});
+
+const server = http.createServer(app);
 
 server.listen(3000, console.log("Server is running at localhost:3000"));
