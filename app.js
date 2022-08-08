d2374f5442863f7644f51d4721fa3237feaa2874
Vaibhav Vyas <vaibhavvyas32@gmail.com>
2022-08-08 17:14:29 +0530

    added a cleaner way to define path to html, changes in pagenotfound.html

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const adminRoutes = require("./routes/admin");
-
 const shopRoutes = require("./routes/shop");
+
+const rootDir = require("./util/path");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -13,9 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "./", "views", "pageNotFound.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "pageNotFound.html"));
 });
 
 app.listen(3000, console.log("Server is running at localhost"));
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,11 @@ const express = require("express");
 
 const router = express.Router();
 
+const rootDir = require("../util/path");
+
 router.get("/", (req, res, next) => {
   //Default
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
