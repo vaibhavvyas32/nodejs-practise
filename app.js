7af57681822b76e14beff0bf37bc01cc71a79144
Vaibhav Vyas <vaibhavvyas32@gmail.com>
2022-08-16 19:24:12 +0530

    added ejs templating engine, old files cleaned

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.set("view engine", "pug");
-
-const bodyParser = require("body-parser");
-const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const rootDir = require("./util/path");
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, "public"))); //A built-in way in expressjs to serve the files directly instead of sendFile. These files are readable only for the user.
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(rootDir, "views", "pageNotFound.html"));
-  res.status(404).render("pageNotFound", { docTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 });
 
-app.listen(3000, console.log("Server is running at localhost"));
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,22 @@
-const express = require("express");
-const router = express.Router();
 const path = require("path");
 
+const express = require("express");
+
 const rootDir = require("../util/path");
+
+const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
-    docTitle: "Add Product",
+    pageTitle: "Add Product",
     path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
   });
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
