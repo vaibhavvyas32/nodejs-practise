41368273e5a4987ec56017ae5a89d462e8ef9222
Vaibhav Vyas <vaibhavvyas32@gmail.com>
2022-08-17 18:41:00 +0530

    added file system functionality

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const fs = require("fs");
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(xyz) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        xyz([]);
+      }
+      xyz(JSON.parse(fileContent));
+    });
+  }
+};
