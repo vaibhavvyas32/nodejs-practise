9d0e0e21baa30670e83f04ae6f467c7b90130f05
Vaibhav Vyas <vaibhavvyas32@gmail.com>
2022-10-05 11:50:07 +0530

    added delete functionality

--- models/product.js
+++ models/product.js
@@ -59,6 +59,19 @@ class Product {
         console.log(err);
       });
   }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
 module.exports = Product;
